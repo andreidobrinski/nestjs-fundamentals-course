6d9b55b910e9b0f7376a9e1d07253fa54bfed756
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-08-24 11:38:25 -0400

    add whitelist to main.ts

--- coffee/src/coffees/coffees.service.ts
+++ coffee/src/coffees/coffees.service.ts
@@ -34,6 +34,8 @@ export class CoffeesService {
 
   create(createCoffeeDto: any) {
     this.coffees.push(createCoffeeDto);
+
+    return createCoffeeDto;
   }
 
   update(id: string, updateCoffeeDto: any) {
