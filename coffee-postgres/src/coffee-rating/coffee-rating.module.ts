3da79f659d46d34f1ffe8e343535e991249b0971
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-08-29 17:20:06 -0400

    dynamic modules

--- coffee-postgres/src/coffee-rating/coffee-rating.module.ts
+++ coffee-postgres/src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    // dynamic modules
+    // DatabaseModule.register({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    // }),
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
