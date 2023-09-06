90e1a4064c5115cfb2f0f98ed8bd8daf6563b768
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-09-06 16:04:14 -0400

    prefer forRootAsync for typeOrmModule import in app module

--- coffee-postgres/src/app.module.ts
+++ coffee-postgres/src/app.module.ts
@@ -21,15 +21,28 @@ import appConfig from './config/app.config';
       load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.DATABASE_HOST,
+    //   port: +process.env.DATABASE_PORT,
+    //   username: process.env.DATABASE_USER,
+    //   password: process.env.DATABASE_PASSWORD,
+    //   database: process.env.DATABASE_NAME,
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+    // best to load TypeOrm module asyncronously. Otherwise the order in the array matters and this must be after the ConfigModule
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     // TypeOrmModule.forRoot({
     //   type: 'postgres', // type of our database
