941202bbd08273bf30aaa8c02b5581272c086f59
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-09-11 13:55:32 -0400

    add examples for global, controller, route or param scoped pipe

--- coffee-postgres/src/coffees/coffees.controller.ts
+++ coffee-postgres/src/coffees/coffees.controller.ts
@@ -7,16 +7,22 @@ import {
   Patch,
   Post,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 
+// uses validation for entire coffees controller
+// use classes instead of instances for pipes
+// @UsePipes(ValidationPipe)
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  // @UsePipes(ValidationPipe)
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     // const { limit, offset } = paginationQuery;
@@ -35,7 +41,10 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(
+    @Param('id') id: string,
+    @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto, // can pass ValidationPipe directory to Body
+  ) {
     return this.coffeesService.update(id, updateCoffeeDto);
   }
 
