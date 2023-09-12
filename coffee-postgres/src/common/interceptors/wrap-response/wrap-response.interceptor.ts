9de8c96c8b9a4164abee1c938fa12e080922e188
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-09-12 13:34:38 -0400

    modify wrap response interceptor

--- coffee-postgres/src/common/interceptors/wrap-response/wrap-response.interceptor.ts
+++ coffee-postgres/src/common/interceptors/wrap-response/wrap-response.interceptor.ts
@@ -4,17 +4,13 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable()
 export class WrapResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log('before wrap response interceptor');
 
-    return next
-      .handle()
-      .pipe(
-        tap((data) => console.log('after wrap response interceptor', data)),
-      );
+    return next.handle().pipe(map((data) => ({ data })));
   }
 }
