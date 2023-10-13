7c634638de5a2dd870a18bbe9270d2ca792c1385
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-10-13 14:37:34 -0400

    add ApiTags to coffees controller

--- coffee-postgres/src/coffees/coffees.controller.ts
+++ coffee-postgres/src/coffees/coffees.controller.ts
@@ -17,11 +17,12 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { Protocol } from 'src/common/decorators/protocol.decorator';
-import { ApiForbiddenResponse, ApiResponse } from '@nestjs/swagger';
+import { ApiForbiddenResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 // uses validation for entire coffees controller
 // use classes instead of instances for pipes
 // @UsePipes(ValidationPipe)
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
